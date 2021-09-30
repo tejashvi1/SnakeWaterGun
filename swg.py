be5444c07db2117d10590e2f2f0b74763adc58f3
tejashvi1 <69606663+tejashvi1@users.noreply.github.com>
2021-09-30 13:36:31 -0400

    Update swg.py

---
+++ swg.py
@@ -0,0 +1,9 @@
+import random
+
+def game():
+    pass
+
+randomno = random.randint(1,3)
+
+p1 =  input("Computer Turn Snake(S) Water(W) Gun(G) ?")
+p2 =  input("Player's Turn Snake(S) Water(W) Gun(G) ?")
